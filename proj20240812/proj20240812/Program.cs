0efbef731fd8ea4366693ab9dca48055fa2320b5
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-08-12 20:20:49 -0300

    Add files via upload

---
+++ proj20240812/proj20240812/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj20240812
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int nj;
+            do
+            {
+                Console.Write("Quantos jogadores? ");
+                nj = int.Parse(Console.ReadLine());
+            }
+            while (nj < 2 || nj > 5);
+
+            string[] nomes;
+            nomes = new string[nj];
+
+            for(i=0; i<nj; i++)
+            {
+                Console.Write("Digite do nome do {0}º jogador: ", i+1);
+                nomes[i] = Console.ReadLine();
+            }
+
+            for (i = 0; i < nj; i++)
+            {
+                Console.WriteLine("Nome do {0}º jogador: {1}", i + 1, nomes[i]);
+            }
+
+
+
+        }
+    }
+}
